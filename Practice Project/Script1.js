5e89e26722b689762c92b3c1335439d4c399f802
YaseenYo <44540920+YaseenYo@users.noreply.github.com>
2018-12-02 09:59:32 +0530

    Add files via upload

---
+++ Practice Project/Script1.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+function recentClick() {
+    alert("hi maaz");
+
+}
+function enterValue() {
+    var value = document.getElementById('txt1').value;
+    value = parseInt(value) + parseInt(value);
+    alert(value);
+} 
